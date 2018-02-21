35e79245abc4ac3400f725f80b68799da1f7eae7
Daniel Krax <daniel.krax@br.de>
2018-02-21 09:48:05 +0100

    added-fake-migrations

---
+++ migrations/10006-oauth2-clients.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+    up: function (queryInterface, Sequelize) {
+        return new Promise((resolve, reject) => {
+            resolve();
+        });
+    },
+
+    down: function (queryInterface, Sequelize) {
+        return new Promise((resolve, reject) => {
+            resolve();
+        });
+    }
+};
