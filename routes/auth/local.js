eb47a7582c270a788270d86336b3825433439656
Dominique Chion <dominique.chion@rts.ch>
2018-04-18 11:38:12 +0200

    When password is not strong enough, API returns score

--- routes/auth/local.js
+++ routes/auth/local.js
@@ -330,7 +330,8 @@ module.exports = function (app, options) {
             if (!passwordHelper.isStrong(req.body.email, req.body.password)) {
                 res.status(400).json({
                     errors: [{msg: passwordHelper.getWeaknessesMsg(req.body.email, req.body.password, req)}],
-                    password_strength_errors: passwordHelper.getWeaknesses(req.body.email, req.body.password, req)
+                    password_strength_errors: passwordHelper.getWeaknesses(req.body.email, req.body.password, req),
+                    score: passwordHelper.getQuality(req.body.email, req.body.password)
                 });
                 return;
             } else {
