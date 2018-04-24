e29e356624a9fedb23f84827c03546ac0ad71135
Dominique Chion <dominique.chion@rts.ch>
2018-04-24 12:05:30 +0200

    Migration WIP

--- migrate/migrations/20180424120000-login-case-insensitive.js
+++ migrate/migrations/20180424120000-login-case-insensitive.js
@@ -5,25 +5,26 @@ module.exports = {
         if (process.env.DB_TYPE === 'sqlite') {
             return resolve();
         }
-        return queryInterface.getForeignKeyReferencesForTable('LocalLogins').then(function (detail) {
-            var constraint = getRowForColumn('login', details);
-            return queryInterface.removeConstraint('LocalLogins', constraint.constraintName);
-
-        }).then(function () {
-            return queryInterface.addConstraint(
-                'LocalLogins',
-                ['login'],
-                {
-                    type: 'foreign key',
-                    name: 'LocalLogins_login_key',
-                    references: {table: 'Users', fields: [sequelize.fn('lower', sequelize.col('login'))]}
-                }
-            );
-        });
+        return queryInterface.removeConstraint('LocalLogins', 'LocalLogins_login_key')
+            .then(function () {
+                console.log("contraint dropped!")
+                return queryInterface.addConstraint(
+                    'LocalLogins',
+                    ['login'],
+                    {
+                        type: 'foreign key',
+                        name: 'LocalLogins_login_key',
+                        references: {table: 'Users', fields: [sequelize.fn('lower', sequelize.col('login'))]}
+                    }
+                );
+            }).catch(function (e) {
+                console.log('Something went wrong:', e);
+            });
     },
 
     down: function (queryInterface, Sequelize) {
         return resolve();
     }
 
-}
+};
+
