27889a635d66c727a6c7cc0ec81794baf4649339
ibis <peter.kornherr@br.de>
2018-06-13 16:41:00 +0200

    add test

--- test/lib/headers.js
+++ test/lib/headers.js
@@ -16,4 +16,10 @@ describe('Check response for headers', function () {
     it('must not set x-powered-by headers', function() {
       expect(this.res.header['x-powered-by']).to.equal(undefined);
     });
+    it('must use the more-secure-headers', function() {
+      expect(this.res.header['x-xss-protection']).to.equal("1; mode=block");
+      expect(this.res.header['x-content-type-options']).to.equal("nosniff");
+      expect(this.res.header['strict-transport-security']).to.equal("max-age=31536000");
+      expect(this.res.header['content-security-policy']).to.equal("default-src https: \'self\'; script-src https: \'self\'; style-src https: \'self\'; img-src *; frame-src \'self\'; connect-src https:; report-uri /csp_report;");
+    })
 });
