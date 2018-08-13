bc99e40ccf46336642f5b463a3e17fcfb30d4811
Dominique Chion <dominique.chion@rts.ch>
2018-08-13 14:50:54 +0200

    Allow data

--- config.docker.js
+++ config.docker.js
@@ -214,7 +214,8 @@ module.exports = {
         additional_scripts_src: process.env.ADDITIONAL_SCRIPTS_SRC,
         additional_fonts_src: process.env.ADDITIONAL_FONTS_SRC,
         additional_frames_src: process.env.ADDITIONAL_FRAMES_SRC,
-        additional_styles_src: process.env.ADDITIONAL_STYLES_SRC
+        additional_styles_src: process.env.ADDITIONAL_STYLES_SRC,
+        allow_fonts_data: process.env.ALLOW_FONTS_DATA,
     },
     // URL path prefix, e.g., '/myapp'
     urlPrefix: process.env.URL_PREFIX || '',
