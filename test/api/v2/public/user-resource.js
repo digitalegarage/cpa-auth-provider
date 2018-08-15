eb06c8c9adc349af3c3ad2a5800aee29b4b148a2
Dominique Chion <dominique.chion@rts.ch>
2018-08-15 14:43:17 +0200

    2nd test

--- test/api/v2/public/user-resource.js
+++ test/api/v2/public/user-resource.js
@@ -2,10 +2,10 @@
 
 var requestHelper = require('../../../request-helper');
 var initData = require('../setup/init-data');
+var db = require('../../../../models');
 
 
 describe('API-V2 user DELETE', function () {
-
 
     context('with good credential', function () {
         var ctx = this;
@@ -26,6 +26,35 @@ describe('API-V2 user DELETE', function () {
 
         it('should return a success', function () {
             expect(ctx.res.statusCode).equal(204);
+        });
+    });
+
+    context('with good credential', function () {
+        var ctx = this;
+
+        before(initData.resetDatabase);
+
+        before(function (done) {
+            requestHelper.sendRequest(ctx, "/api/v2/public/user", {
+                    method: 'delete',
+                    basicAuth: {
+                        login: initData.USER_1.email,
+                        password: initData.USER_1.password
+                    }
+                },
+                done
+            );
+        });
+
+        before(function (done) {
+            db.LocalLogin.findOne({where: {login: initData.USER_1.email}}).then(function (ll) {
+               ctx.ll = ll;
+               done();
+            });
+        });
+
+        it('should drop the user', function () {
+            expect(ctx.ll).to.be.null;
         });
     });
 
