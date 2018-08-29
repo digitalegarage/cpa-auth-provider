68261c0e9494f1f242f72a747dbbd96c35853f9d
Dominique Chion <dominique.chion@rts.ch>
2018-08-29 09:31:09 +0200

    Swagger

--- routes/api/v2/auth/login-resource.js
+++ routes/api/v2/auth/login-resource.js
@@ -4,9 +4,49 @@ var passport = require('passport');
 var afterLoginHelper = require('../../../../lib/afterlogin-helper');
 var cors = require('../../../../lib/cors');
 var config = require('../../../../config');
+var logger = require('../../../../lib/logger');
 
 
 module.exports = function (app, options) {
+
+    /**
+     * @swagger
+     * definitions:
+     *   Credentials:
+     *      type: "object"
+     *      properties:
+     *           email:
+     *               type: "string"
+     *               example: "someone@domain.org"
+     *               description: "user email"
+     *           password:
+     *               type: "string"
+     *               example: "myVeryStrongPassword"
+     *               description: "user password"
+     *
+     *
+     */
+
+
+    /**
+     * @swagger
+     * /api/v2/session/login:
+     *   post:
+     *     description: login
+     *     operationId: "sessionLogin"
+     *     content:
+     *        - application/json
+     *     parameters:
+     *          - in: body
+     *            name: "credentials"
+     *            description: "user credential"
+     *            required: true
+     *            schema:
+     *              $ref: "#/definitions/Credentials"
+     *     responses:
+     *          "200":
+     *            description: "login succeed"
+     */
     app.post('/api/v2/session/login', cors,
         passport.authenticate('local', {session: true}),
         function (req, res, next) {
@@ -20,6 +60,7 @@ module.exports = function (app, options) {
             let headers = res.getHeader("set-cookie");
             var token;
             for (var header in headers) {
+                logger.debug("parsing ", headers[header]);
                 if (headers[header].indexOf(config.auth_session_cookie.name) == 0) {
                     var attributes = headers[header].split(';');
                     for (var attribute in attributes) {
@@ -30,6 +71,11 @@ module.exports = function (app, options) {
                     }
                     break;
                 }
+            }
+
+            // server doesn't set cookie (cookie is in the request). => respond with request one
+            if (!token) {
+                token = req.cookies[config.auth_session_cookie.name];
             }
 
             res.write('{"token": "' + token + '"}');
