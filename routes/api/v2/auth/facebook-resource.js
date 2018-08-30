d67754beb1483f23732a7c16b9727ad41ae7b4ea
Dominique Chion <dominique.chion@rts.ch>
2018-08-30 13:40:11 +0200

    Fix tests

--- routes/api/v2/auth/facebook-resource.js
+++ routes/api/v2/auth/facebook-resource.js
@@ -8,12 +8,13 @@ var passport = require('passport');
 
 var REQUESTED_PERMISSIONS = ['email', 'user_birthday'];
 
-passport.use(facebookHelper.getFacebookStrategy('/api/v2/auth/facebook/callback'));
+const FACEBOOK_STRATEGY_NAME = 'facebookRedirect';
+passport.use(FACEBOOK_STRATEGY_NAME, facebookHelper.getFacebookStrategy('/api/v2/auth/facebook/callback'));
 
 module.exports = function (app, options) {
-    app.get('/api/v2/auth/facebook', passport.authenticate('facebook', {scope: REQUESTED_PERMISSIONS}));
+    app.get('/api/v2/auth/facebook', passport.authenticate(FACEBOOK_STRATEGY_NAME, {scope: REQUESTED_PERMISSIONS}));
 
-    app.get('/api/v2/auth/facebook/callback', passport.authenticate('facebook', {failureRedirect: config.urlPrefix + '/auth?error=LOGIN_INVALID_EMAIL_BECAUSE_NOT_VALIDATED_FB'}), function (req, res) {
+    app.get('/api/v2/auth/facebook/callback', passport.authenticate(FACEBOOK_STRATEGY_NAME, {failureRedirect: config.urlPrefix + '/auth?error=LOGIN_INVALID_EMAIL_BECAUSE_NOT_VALIDATED_FB'}), function (req, res) {
 
         return res.redirect(requestHelper.getPath('/api/v2/session/cookie?redirect=' + config.baseUrl + '/user/profile'));
 
