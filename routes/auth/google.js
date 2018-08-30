b11cf70740546adff39de1428a60808129915718
Dominique Chion <dominique.chion@rts.ch>
2018-08-30 10:47:06 +0200

    Extract google and facebook strategy

--- routes/auth/google.js
+++ routes/auth/google.js
@@ -1,54 +1,12 @@
 "use strict";
 
-var db = require('../../models/index');
 var config = require('../../config');
 var requestHelper = require('../../lib/request-helper');
-var socialLoginHelper = require('../../lib/social-login-helper');
 var googleHelper = require('../../lib/google-helper');
 
 var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth20');
 
-var callbackHelper = require('../../lib/callback-helper');
-
-passport.use(new GoogleStrategy({
-        clientID: config.identity_providers.google.client_id,
-        clientSecret: config.identity_providers.google.client_secret,
-        callbackURL: callbackHelper.getURL('/auth/google/callback')
-    },
-    function (accessToken, refreshToken, profile, done) {
-        var email = '';
-        if (profile.emails !== undefined) {
-            email = profile.emails[0].value;
-        }
-
-        if (email === '') {
-            return done(new Error('NO_EMAIL', null));
-        }
-
-        var providerUid = googleHelper.buildGoogleId(profile.id);
-
-        return socialLoginHelper.findOrCreateSocialLoginUser(socialLoginHelper.GOOGLE, email, providerUid, profile.displayName, profile.name.givenName, profile.name.familyName, profile.gender, null).then(
-            function (user) {
-                if (user) {
-                    db.SocialLogin.findOne({
-                        where: {
-                            user_id: user.id,
-                            name: socialLoginHelper.GOOGLE
-                        }
-                    }).then(function (socialLogin) {
-                        socialLogin.logLogin(user);
-                    });
-                }
-                return done(null, user);
-            }
-        ).catch(
-            function (err) {
-                done(err);
-            }
-        );
-    }
-));
+passport.use(googleHelper.getGoogleStrategy('/auth/google/callback'));
 
 module.exports = function (app, options) {
     app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
