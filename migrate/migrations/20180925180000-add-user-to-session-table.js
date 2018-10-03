85c22ff6dea4afe5e798f125a237464e6acb8b50
Peter Kornherr <ibis@br.de>
2018-10-03 18:00:52 +0200

    check for "Sessions" table while migrating

--- migrate/migrations/20180925180000-add-user-to-session-table.js
+++ migrate/migrations/20180925180000-add-user-to-session-table.js
@@ -7,22 +7,33 @@ module.exports = {
             if (process.env.DB_TYPE === 'sqlite')
                 resolve();
             else {
-                queryInterface.addColumn('Sessions','userId',{type: Sequelize.STRING, allowNull: true})
-                .then(() => {
-                    return queryInterface.sequelize.query('update public."Sessions" set "userId" = data::json->\'passport\'->\'user\'')
-                    .then(function(rows) {
-                        return;
-                    })
-                    .catch(function(e) {
-                        reject(e);
-                    });
-                })
-                .then(() => {
-                    queryInterface.sequelize.query('﻿CREATE INDEX Sessions_userid_idx ON public."Sessions" ("userId");');
-                    resolve();
+                queryInterface.sequelize.query('SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = \'Sessions\')')
+                .then((res) => {
+                    if (res[0][0].exists === true) {
+                        queryInterface.addColumn('Sessions','userId',{type: Sequelize.STRING, allowNull: true})
+                        .then(() => {
+                            return queryInterface.sequelize.query('update public."Sessions" set "userId" = data::json->\'passport\'->\'user\'')
+                            .then(function(rows) {
+                                return;
+                            })
+                            .catch(function(e) {
+                                reject(e);
+                            });
+                        })
+                        .then(() => {
+                            queryInterface.sequelize.query('﻿CREATE INDEX Sessions_userid_idx ON public."Sessions" ("userId");');
+                            resolve();
+                        })
+                        .catch((e) => {
+                            reject(e);
+                        });
+                    } else {
+                        // There is no "Sessions" table, so no altering. Sequelize will create dynamically.
+                        resolve();
+                    }
                 })
                 .catch((e) => {
-                    reject(e);
+                    console.console.error(e);
                 });
             }
         });
