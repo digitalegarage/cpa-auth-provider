6dec114b02641baa8ebc8585bf4e290ff5c5af61
Dominique Chion <dominique.chion@rts.ch>
2018-12-07 14:35:56 +0100

    Some cleaning + retrieve user profile

--- routes/api/v2/auth/facebook-resource.js
+++ routes/api/v2/auth/facebook-resource.js
@@ -42,11 +42,8 @@ module.exports = function(app, options) {
         };
 
         return request(options).then(function(tokenJsonResponse) {
-            const accessToken = tokenJsonResponse.access_token;
-
-            validateTokenAndLog(accessToken, res, req);
-        }).
-        catch(function(err) {
+            validateTokenAndLog(tokenJsonResponse.access_token, res, req);
+        }).catch(function(err) {
             logger.info('An error occured while requesting the token', err);
             return res.json({error: 'An error occured while requesting the token'}).status(401).send();
         });
@@ -64,7 +61,10 @@ module.exports = function(app, options) {
 };
 
 function validateTokenAndLog(accessToken, res, req) {
-    var options = {
+
+    // Step 1: validate access token
+
+    let options = {
         uri: 'https://graph.facebook.com/debug_token?' +
             'input_token=' + accessToken +
             '&access_token=' + config.identity_providers.facebook.client_id + '|' + config.identity_providers.facebook.client_secret,
@@ -75,22 +75,56 @@ function validateTokenAndLog(accessToken, res, req) {
         if (!jsonResponse.data || !jsonResponse.data.user_id) {
             return res.json({error: 'An error occured while validating the token'}).status(401).send();
         } else {
-            socialLoginHelper.findOrCreateSocialLoginUser(socialLoginHelper.FB, null, jsonResponse.data.user_id, null, null, null, null, null)  //FIXME <===============================
-            .then(function(user) {
-                if (user) {
-                    db.SocialLogin.findOne({
-                        where: {
-                            user_id: user.id,
-                            name: socialLoginHelper.FB,
-                        },
-                    }).then(function() {
-                        req.logIn(user, function() {
-                            res.sendStatus(204);
+
+            // Step 2: request user profile to graph API
+
+            let options = {
+                uri: 'https://graph.facebook.com/v3.2/me?' +
+                    'fields=id,name,email,first_name,last_name,gender' +
+                    '&access_token=' + accessToken,
+                json: true,
+            };
+            request(options).then(function(profile) {
+
+                // Step 3: find or create user in IDP Db
+
+                socialLoginHelper.findOrCreateSocialLoginUser(
+                    socialLoginHelper.FB,
+                    profile.email,
+                    jsonResponse.data.user_id,
+                    profile.name,
+                    profile.first_name,
+                    profile.last_name,
+                    profile.gender,
+                    null) // Gender is null "The field 'birthday' is only accessible on the User object after the user grants the 'user_birthday' permission."
+                .then(function(user) {
+                    if (user) {
+                        db.SocialLogin.findOne({
+                            where: {
+                                user_id: user.id,
+                                name: socialLoginHelper.FB,
+                            },
+                        }).then(function(socialLogin) {
+                            // Step 4: Log last login
+                            socialLogin.logLogin(user);
+
+                            // Step 5: Finally log the user
+                            req.logIn(user, function() {
+                                res.sendStatus(204);
+                            });
                         });
-                    });
-                } else {
-                    return res.json({error: 'An error occured while validating the token'}).status(401).send();
-                }
+                    } else {
+                        return res.json({error: 'An error occurred while validating the token'}).status(401).send();
+                    }
+                }).catch(function(err) {
+                    logger.info('An error occurred while saving user in IDP db', err);
+                    return res.json({error: 'An error occurred while saving user in IDP db'}).status(401).send();
+
+                });
+            }).catch(function(err) {
+                logger.info('An error occurred while retrieving user data using the token', err);
+                return res.json({error: 'An error occurred while retrieving user data using the token'}).status(401).send();
+
             });
         }
     }).catch(function(err) {
