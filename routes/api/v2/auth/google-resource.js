0c4dda4caaa5d3ace506d0d2c707cbe9e688a82a
Dominique Chion <dominique.chion@rts.ch>
2019-01-31 12:08:04 +0100

    API to login with google token

--- routes/api/v2/auth/google-resource.js
+++ routes/api/v2/auth/google-resource.js
@@ -4,6 +4,8 @@ const cors = require('../../../../lib/cors');
 var config = require('../../../../config');
 var socialLoginHelper = require('../../../../lib/social-login-helper');
 var googleHelper = require('../../../../lib/google-helper');
+const afterLoginHelper = require('../../../../lib/afterlogin-helper');
+var db = require('../../../../models/index');
 
 var passport = require('passport');
 
@@ -24,4 +26,94 @@ module.exports = function (app, options) {
         socialLoginHelper.afterSocialLoginSucceed(req, res);
 
     });
+
+    /**
+     * @swagger
+     * definitions:
+     *   GoogleTokenData:
+     *      type: "object"
+     *      properties:
+     *           token:
+     *               type: "string"
+     *               example: "eyJhbGciOiJSUzI1NiIsImtpZCI6IjZmYjA1Zjc0MjM2NmVlNGNmNGJjZjQ5Zjk4NGM0OD..."
+     *               description: "A google oAuth code"
+     *
+     */
+
+
+    /**
+     * @swagger
+     * /api/v2/auth/google/token:
+     *   post:
+     *     description: log user (session) using FB code
+     *     tags: [AUTH]
+     *     content:
+     *        - application/json
+     *     parameters:
+     *          - in: body
+     *            name: "Google token"
+     *            description: "Google token data"
+     *            required: true
+     *            schema:
+     *              $ref: "#/definitions/GoogleTokenData"
+     *     responses:
+     *          "204":
+     *            description: "login succeed"
+     */
+    app.options('/api/v2/auth/google/token', cors);
+    app.post('/api/v2/auth/google/token', function(req, res) {
+        if (!req.body.token){//} || !req.body.redirect_uri) {
+            // return res.status(400).json({error: 'missing code and/or redirect_uri in request body'}).send();
+            return res.status(400).json({error: 'missing code in request body'}).send();
+        }
+        authViaToken(req.body.token, req, res);
+
+    });
+
+
+    function authViaToken(token, req, res) {
+        return new Promise(function(resolve, reject) {
+
+            // Step 1: verify token
+            return googleHelper.verifyGoogleIdToken(token).then((gmailUser) => {
+
+                // Step 2: find or create user in IDP Db
+                socialLoginHelper.findOrCreateSocialLoginUser(
+                    socialLoginHelper.GOOGLE,
+                    gmailUser.email,
+                    gmailUser.provider_uid,
+                    gmailUser.display_name,
+                    null,
+                    null,
+                    null,
+                    null).then(function(user) {
+                    if (user) {
+                        db.SocialLogin.findOne({
+                            where: {
+                                user_id: user.id,
+                                name: socialLoginHelper.GOOGLE
+                            }
+                        }).then(function(socialLogin) {
+                            // Step 3: Log last login
+                            socialLogin.logLogin(user);
+
+                            // Step 4: Finally log the user
+                            req.logIn(user, function() {
+                                res.sendStatus(204);
+                            });
+                        });
+                    } else {
+                        return res.status(401).json({error: 'An error occurred while validating the token'}).send();
+                    }
+                }).catch(function(err) {
+                    logger.info('An error occurred while saving user in IDP db', err);
+                    return res.status(401).json({error: 'An error occurred while saving user in IDP db'}).send();
+
+                });
+            }).catch(function(err) {
+                reject(err);
+            });
+        });
+    }
+
 };
