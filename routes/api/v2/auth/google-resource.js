07ce59c984df47820c6044704514a2bf9ec9064f
Dominique Chion <dominique.chion@rts.ch>
2019-01-31 16:35:56 +0100

    Google AJAX login via code

--- routes/api/v2/auth/google-resource.js
+++ routes/api/v2/auth/google-resource.js
@@ -38,6 +38,17 @@ module.exports = function (app, options) {
      *               type: "string"
      *               example: "eyJhbGciOiJSUzI1NiIsImtpZCI6IjZmYjA1Zjc0MjM2NmVlNGNmNGJjZjQ5Zjk4NGM0OD..."
      *               description: "A google oAuth code"
+     *   GoogleCodeData:
+     *      type: "object"
+     *      properties:
+     *           code:
+     *               type: "string"
+     *               example: "AQDubYxjE3f9eKc5giY5rT1m8Cfumx3Fyb-UVwrHglj_R..."
+     *               description: "A google oAuth code"
+     *           redirect_uri:
+     *               type: "string"
+     *               example: "http://demo-peach.ebu.io/idp/api/v2/auth/google/callback"
+     *               description: "Redirect uri that had been used to get the code"
      *
      */
 
@@ -46,7 +57,7 @@ module.exports = function (app, options) {
      * @swagger
      * /api/v2/auth/google/token:
      *   post:
-     *     description: log user (session) using FB code
+     *     description: log user (session) using Google code
      *     tags: [AUTH]
      *     content:
      *        - application/json
@@ -71,49 +82,85 @@ module.exports = function (app, options) {
     });
 
 
+    /**
+     * @swagger
+     * /api/v2/auth/google/code:
+     *   post:
+     *     description: log user (session) using Google code
+     *     tags: [AUTH]
+     *     content:
+     *        - application/json
+     *     parameters:
+     *          - in: body
+     *            name: "Google token"
+     *            description: "Google token data"
+     *            required: true
+     *            schema:
+     *              $ref: "#/definitions/GoogleCodeData"
+     *     responses:
+     *          "204":
+     *            description: "login succeed"
+     */
+    app.options('/api/v2/auth/google/code', cors);
+    app.post('/api/v2/auth/google/code', function(req, res) {
+        if (!req.body.code || !req.body.redirect_uri) {
+            return res.status(400).json({error: 'missing code and/or redirect_uri in request body'}).send();
+        }
+        authViaCode(req.body.code, req.body.redirect_uri, req, res);
+    });
+
+    function authViaCode(code, redirectUri, req, res) {
+        googleHelper.getGoogleToken(code, redirectUri).then(function(token) {
+            if (!token) {
+                return res.status(401).json({error: 'Cannot authenticate with google'}).send();
+            } else {
+                authViaToken(token, req, res);
+            }
+        });
+    }
+
+
     function authViaToken(token, req, res) {
-        return new Promise(function(resolve, reject) {
+        // Step 1: verify token
+        return googleHelper.verifyGoogleIdToken(token).then((gmailUser) => {
 
-            // Step 1: verify token
-            return googleHelper.verifyGoogleIdToken(token).then((gmailUser) => {
+            // Step 2: find or create user in IDP Db
+            socialLoginHelper.findOrCreateSocialLoginUser(
+                socialLoginHelper.GOOGLE,
+                gmailUser.email,
+                gmailUser.provider_uid,
+                gmailUser.display_name,
+                null,
+                null,
+                null,
+                null).then(function(user) {
+                if (user) {
+                    db.SocialLogin.findOne({
+                        where: {
+                            user_id: user.id,
+                            name: socialLoginHelper.GOOGLE
+                        }
+                    }).then(function(socialLogin) {
+                        // Step 3: Log last login
+                        socialLogin.logLogin(user);
+                        afterLoginHelper.afterLogin(user, gmailUser.email, res);
 
-                // Step 2: find or create user in IDP Db
-                socialLoginHelper.findOrCreateSocialLoginUser(
-                    socialLoginHelper.GOOGLE,
-                    gmailUser.email,
-                    gmailUser.provider_uid,
-                    gmailUser.display_name,
-                    null,
-                    null,
-                    null,
-                    null).then(function(user) {
-                    if (user) {
-                        db.SocialLogin.findOne({
-                            where: {
-                                user_id: user.id,
-                                name: socialLoginHelper.GOOGLE
-                            }
-                        }).then(function(socialLogin) {
-                            // Step 3: Log last login
-                            socialLogin.logLogin(user);
-                            afterLoginHelper.afterLogin(user, gmailUser.email, res);
-
-                            // Step 4: Finally log the user
-                            req.logIn(user, function() {
-                                res.sendStatus(204);
-                            });
+                        // Step 4: Finally log the user
+                        req.logIn(user, function() {
+                            res.sendStatus(204);
                         });
-                    } else {
-                        return res.status(401).json({error: 'An error occurred while validating the token'}).send();
-                    }
-                }).catch(function(err) {
-                    logger.info('An error occurred while saving user in IDP db', err);
-                    return res.status(401).json({error: 'An error occurred while saving user in IDP db'}).send();
-
-                });
+                    });
+                } else {
+                    return res.status(401).json({error: 'An error occurred while validating the token'}).send();
+                }
             }).catch(function(err) {
-                reject(err);
+                logger.info('An error occurred while saving user in IDP db', err);
+                return res.status(401).json({error: 'An error occurred while saving user in IDP db'}).send();
+
             });
+        }).catch(function(err) {
+            logger.info('An error occurred while verifying google token', err);
+            return res.status(401).json({error: 'An error occurred while verifying google token'}).send();
         });
     }
 
