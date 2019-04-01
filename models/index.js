cf3d1845deae3eca4eac9d7f486c1750b25f743e
m19c <marcandrebinder@gmail.com>
2019-04-01 15:01:24 +0200

    change poolsize to 30

--- models/index.js
+++ models/index.js
@@ -1,13 +1,17 @@
 "use strict";
 
-var fs = require('fs');
-var path = require('path');
-var Sequelize = require('sequelize');
-var lodash = require('lodash');
+var fs = require("fs");
+var path = require("path");
+var Sequelize = require("sequelize");
+var lodash = require("lodash");
 var db = {};
-var config = require('../config');
+var config = require("../config");
 
-var sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
+var sequelize = new Sequelize(
+  config.db.database,
+  config.db.user,
+  config.db.password,
+  {
     dialect: config.db.type,
     dialectOptions: config.db.dialectOptions,
     host: config.db.host,
@@ -16,24 +20,31 @@ var sequelize = new Sequelize(config.db.database, config.db.user, config.db.pass
     logging: config.db.debug ? console.log : false,
     operatorsAliases: false,
     pool: {
-        max: 50,
-        min: 5,
-        acquire: 30000,
-        idle: 10000
+      // http://docs.sequelizejs.com/manual/getting-started
+      max: 30,
+      min: 5,
+      acquire: 30000,
+      idle: 10000
     }
-});
+  }
+);
 
-fs.readdirSync(__dirname).filter(function (file) {
-    return (file.indexOf('.') !== 0) && (file !== 'index.js');
-}).forEach(function (file) {
+fs.readdirSync(__dirname)
+  .filter(function(file) {
+    return file.indexOf(".") !== 0 && file !== "index.js";
+  })
+  .forEach(function(file) {
     var model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
+  });
+
+Object.keys(db).forEach(function(modelName) {
+  if (db[modelName].options.hasOwnProperty("associate")) {
+    db[modelName].options.associate(db);
+  }
 });
 
-Object.keys(db).forEach(function (modelName) {
-    if (db[modelName].options.hasOwnProperty('associate')) {
-        db[modelName].options.associate(db);
-    }
-});
-
-module.exports = lodash.extend({sequelize: sequelize, Sequelize: Sequelize}, db);
+module.exports = lodash.extend(
+  { sequelize: sequelize, Sequelize: Sequelize },
+  db
+);
