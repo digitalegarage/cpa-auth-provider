888a407f242329349d9a04ee13fcaa400ecaa407
Dominique Chion <dominique.chion@rts.ch>
2019-05-03 09:55:59 +0200

    Linter

--- test/lib/headers.js
+++ test/lib/headers.js
@@ -23,6 +23,6 @@ describe('Check response for headers', function () {
       expect(this.res.header['content-security-policy']).to.equal("default-src data: https: 'self'; script-src https: 'self' 'unsafe-inline' http://connect.facebook.com/ https://broadcarster.org; style-src https: 'self' 'unsafe-inline' https://broadcarster.org; img-src *; frame-src 'self' http://staticxx.facebook.com https://www.google.com https://accounts.google.com/ https://broadcarster.org; connect-src https:; font-src 'self' https://broadcarster.org");
     });
     it('health check endpoint must not contain session cookie', function() {
-      expect(this.res.header['set-cookie']).to.be.undefined;
+      expect(this.res.header['set-cookie']).to.equal(undefined);
     });
 });
