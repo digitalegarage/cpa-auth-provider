6356ce7af2c1e39268050461e19f9d3c1300086a
Dominique Chion <dominique.chion@rts.ch>
2019-05-13 14:37:25 +0200

    Clean docker config

--- migrate/migrations_config.js
+++ migrate/migrations_config.js
@@ -1,22 +1,22 @@
 module.exports = {
     "development": {
         "host": process.env.DB_HOST,
-        "dialect": process.env.DB_TYPE,
+        "dialect": process.env.DB_TYPE || 'mysql',
         "dialectOptions": process.env.DB_DIALECT_OPTIONS ? JSON.parse(process.env.DB_DIALECT_OPTIONS) : undefined,
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_DATABASE,
+        "database": process.env.DB_DATABASE || 'idp',
         "port": process.env.DB_PORT,
         "storage": process.env.DB_FILENAME,
         "logging": console.log
     },
     "production": {
         "host": process.env.DB_HOST,
-        "dialect": process.env.DB_TYPE,
+        "dialect": process.env.DB_TYPE || 'mysql',
         "dialectOptions": process.env.DB_DIALECT_OPTIONS ? JSON.parse(process.env.DB_DIALECT_OPTIONS) : undefined,
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_DATABASE,
+        "database": process.env.DB_DATABASE || 'idp',
         "port": process.env.DB_PORT,
         "storage": process.env.DB_FILENAME,
         "logging": console.log
