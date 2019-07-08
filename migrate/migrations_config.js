7d47d3156e193617506fbe34cb5dd2af4645ee06
Markus Heimbuchner <markusderheimbuchner@gmail.com>
2019-07-08 14:17:19 +0200

    config migration for ssl

--- migrate/migrations_config.js
+++ migrate/migrations_config.js
@@ -1,8 +1,28 @@
+const fs = require('fs');
+
+console.log("HELLOOOO");
+
+let dialectOptions = process.env.DB_DIALECT_OPTIONS;
+
+console.log(dialectOptions);
+
+if (dialectOptions) {
+    const dopts = JSON.parse(process.env.DB_DIALECT_OPTIONS);
+    if (dopts.ssl && (!dopts.ssl.key || !dopts.ssl.cert || !dopts.ca)) {
+        console.error("not all cert files");
+        process.exit(-1);
+    } else {
+        dopts.ssl.key = fs.readFileSync(dopts.ssl.key);
+        dopts.ssl.cert = fs.readFileSync(dopts.ssl.cert);
+        dopts.ssl.ca = fs.readFileSync(dopts.ssl.ca);
+    }
+}
+
 module.exports = {
     "development": {
         "host": process.env.DB_HOST,
         "dialect": process.env.DB_TYPE,
-        "dialectOptions": process.env.DB_DIALECT_OPTIONS ? JSON.parse(process.env.DB_DIALECT_OPTIONS) : undefined,
+        "dialectOptions": dialectOptions,
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_DATABASE,
@@ -13,7 +33,7 @@ module.exports = {
     "production": {
         "host": process.env.DB_HOST,
         "dialect": process.env.DB_TYPE,
-        "dialectOptions": process.env.DB_DIALECT_OPTIONS ? JSON.parse(process.env.DB_DIALECT_OPTIONS) : undefined,
+        "dialectOptions": dialectOptions,
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
         "database": process.env.DB_DATABASE,
