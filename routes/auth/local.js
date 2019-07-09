d8e829eabda440059b01e31f53e9203431ce3256
Dominique Chion <dominique.chion@rts.ch>
2019-07-09 14:47:46 +0200

    restore /email_verify

--- routes/auth/local.js
+++ routes/auth/local.js
@@ -48,4 +48,42 @@ module.exports = function (app, options) {
         });
     });
 
+    app.get('/email_verify', function (req, res, next) {
+
+        finder.findUserByLocalAccountEmail(req.query.email).then(function (localLogin) {
+            if (localLogin) {
+                codeHelper.verifyEmail(localLogin, req.query.code).then(function (success) {
+                        if (success) {
+                            if (config.broadcaster.changeEmailConfirmationPage) {
+                                if (config.broadcaster.changeEmailConfirmationPage.indexOf('?') >= 0) {
+                                    return res.redirect(config.broadcaster.changeEmailConfirmationPage + '&success=true');
+                                } else {
+                                    return res.redirect(config.broadcaster.changeEmailConfirmationPage + '?success=true');
+                                }
+                            } else {
+                                    res.render('./verify-mail.ejs', {
+                                        verified: localLogin.verified,
+                                        userId: localLogin.user_id
+                                    });
+                            }
+                        } else {
+                            if (config.broadcaster.changeEmailConfirmationPage) {
+                                if (config.broadcaster.changeEmailConfirmationPage.indexOf('?') >= 0) {
+                                    return res.redirect(config.broadcaster.changeEmailConfirmationPage + '&success=false');
+                                } else {
+                                    return res.redirect(config.broadcaster.changeEmailConfirmationPage + '?success=false');
+                                }
+                            } else {
+                                res.render('./verify-mail.ejs', {verified: false});
+                            }
+                        }
+                    }
+                );
+            } else {
+                return res.status(400).json({msg: req.__('BACK_SIGNUP_EMAIL_VERIFY_USER_NOT_FOUND')});
+            }
+        }, function (error) {
+            next(error);
+        });
+    });
 };
