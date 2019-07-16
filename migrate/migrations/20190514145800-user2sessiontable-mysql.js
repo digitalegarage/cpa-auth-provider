fc5b9519fc7af156b0c96dfd60ec077dfa0a0a4f
Peter Kornherr <peter@kornherr.net>
2019-07-16 16:51:23 +0200

    wip

--- migrate/migrations/20190514145800-user2sessiontable-mysql.js
+++ migrate/migrations/20190514145800-user2sessiontable-mysql.js
@@ -8,37 +8,38 @@ module.exports = {
                 resolve();
             else {
                 // check if Table "Sessions" is present
-                queryInterface.sequelize.query('SHOW TABLES LIKE \'Sessions\'')
-                .then((result,meta) => {
-                    if (result && result.length && result[0].length === 0) {
-                        // NOP. Table doesn't exist, let sequelize create it. This will contain userId, so no need to update here.
-                        resolve();
-                    } else {
-                        queryInterface.sequelize.query('SELECT * from information_schema.COLUMNS WHERE TABLE_NAME = \'Sessions\' AND COLUMN_NAME = \'userId\'')
-                        .then(r => {
-                            if (r[0].length === 0) {
-                                // column userId does not exist. Create:
-                                queryInterface.addColumn('Sessions', 'userId', {type: Sequelize.STRING(255)})
-                                .then(() => {
-                                    // update userId column with data from session/passport
-                                    queryInterface.sequelize.query('UPDATE Sessions set userId = JSON_EXTRACT(`data`, \'$.passport.user\')')
-                                    .then(() => {
-                                        resolve();
-                                    })
-                                });
-                            } else {
-                                // column userId exists. Fill it. Dup because of async row creation.
-                                queryInterface.sequelize.query('UPDATE Sessions set userId = JSON_EXTRACT(`data`, \'$.passport.user\')')
-                                .then(() => {
-                                    resolve();
-                                });
-                            }
-                        });
-                    }
-                })
-                .catch(e => {
-                    reject(e);
-                });
+//                queryInterface.sequelize.query('SHOW TABLES LIKE \'Sessions\'')
+//                .then((result,meta) => {
+//                    if (result && result.length && result[0].length === 0) {
+//                        // NOP. Table doesn't exist, let sequelize create it. This will contain userId, so no need to update here.
+//                        resolve();
+//                    } else {
+//                        queryInterface.sequelize.query('SELECT * from information_schema.COLUMNS WHERE TABLE_NAME = \'Sessions\' AND COLUMN_NAME = \'userId\'')
+//                        .then(r => {
+//                            if (r[0].length === 0) {
+//                                // column userId does not exist. Create:
+//                                queryInterface.addColumn('Sessions', 'userId', {type: Sequelize.STRING(255)})
+//                                .then(() => {
+//                                    // update userId column with data from session/passport
+//                                    queryInterface.sequelize.query('UPDATE Sessions set userId = JSON_EXTRACT(`data`, \'$.passport.user\')')
+//                                    .then(() => {
+//                                        resolve();
+//                                    })
+//                                });
+//                            } else {
+//                                // column userId exists. Fill it. Dup because of async row creation.
+//                                queryInterface.sequelize.query('UPDATE Sessions set userId = JSON_EXTRACT(`data`, \'$.passport.user\')')
+//                                .then(() => {
+//                                    resolve();
+//                                });
+//                            }
+//                        });
+//                    }
+//                })
+//                .catch(e => {
+//                    reject(e);
+//                });
+                resolve();
             }
         });
     },
