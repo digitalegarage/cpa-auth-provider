1f464081622c9e111f455369e93524aa8f714a2a
Dominique Chion <dominique.chion@rts.ch>
2019-07-17 10:12:07 +0200

    Cleanning

--- routes/api/local.js
+++ routes/api/local.js
@@ -1,39 +1,6 @@
 "use strict";
 
-var db = require('../../models');
-var config = require('../../config');
-
-var passport = require('passport');
-
-var JwtStrategy = require('passport-jwt').Strategy,
-    ExtractJwt = require('passport-jwt').ExtractJwt;
-
-//TODO move or delete the following
-var opts = {};
-opts.jwtFromRequest = ExtractJwt.fromExtractors(
-    [
-        ExtractJwt.fromAuthHeaderWithScheme('JWT'),
-        ExtractJwt.fromAuthHeaderAsBearerToken()
-    ]
-);
-opts.secretOrKey = config.jwtSecret;
-// opts.issuer = "accounts.examplesoft.com";
-// opts.audience = "yoursite.net";
-passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
-    if (!jwt_payload) {
-        done(null, false);
-        return;
-    }
-    db.User.findOne({where: {id: jwt_payload.id}, include: [db.LocalLogin]})
-        .then(function (user) {
-            if (user) {
-                done(null, user);
-            } else {
-                done(null, false);
-            }
-        });
-}));
-
+// This file is needed because local login require both route/api/local.js and route/auth/local.js
 module.exports = function (app, options) {
 
 };
