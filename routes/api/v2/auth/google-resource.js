2a11298d815568eca9c224d4844ecbdd1385de1d
Andrew Dry <andrew.t.dry@gmail.com>
2020-01-15 15:53:42 +0100

    google helper and resource upfated

--- routes/api/v2/auth/google-resource.js
+++ routes/api/v2/auth/google-resource.js
@@ -159,7 +159,7 @@ module.exports = function (app, options) {
                         reject(err);
                     });
                 }
-            });
+            }).catch((err) => {reject(err)});
         });
     }
 
@@ -174,7 +174,7 @@ module.exports = function (app, options) {
                     socialLoginHelper.GOOGLE,
                     gmailUser.email,
                     gmailUser.provider_uid,
-                    gmailUser.display_name,
+                    null, //gmailUser.display_name,
                     null,
                     null,
                     null,
